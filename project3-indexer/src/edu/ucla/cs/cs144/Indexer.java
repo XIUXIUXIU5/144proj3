004793932d2ab86314747c44fe0b9cc82dab388b
XIUXIUXIU5 <shaolei@ucla.edu>
2016-02-08 21:47:54 -0800

    create index

--- project3-indexer/src/edu/ucla/cs/cs144/Indexer.java
+++ project3-indexer/src/edu/ucla/cs/cs144/Indexer.java
@@ -23,7 +23,7 @@ import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
 
 public class Indexer {
-    
+
     /** Creates a new instance of Indexer */
     public Indexer() {
     }
@@ -32,72 +32,75 @@ public class Indexer {
 
     public IndexWriter getIndexWriter(boolean create) throws IOException {
         if (indexWriter == null) {
-            Directory indexDir = FSDirectory.open(new File("index-directory"));
+            Directory indexDir = FSDirectory.open(new File("var/lib/lucene/index-directory"));
             IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_4_10_2, new StandardAnalyzer());
             indexWriter = new IndexWriter(indexDir, config);
         }
         return indexWriter;
-   }
+    }
 
     public void closeIndexWriter() throws IOException {
         if (indexWriter != null) {
             indexWriter.close();
         }
-   }
- 
-    public void indexItem(Hotel hotel) throws IOException {
-    
     }
 
-    public void rebuildIndexes() {
+    public void indexItem(ResultSet item, String category) throws IOException, SQLException {
+
+        IndexWriter writer = getIndexWriter(false);
+        Document doc = new Document();
+        int id = item.getInt("ItemID");
+        doc.add(new StringField("id", Integer.toString(id), Field.Store.YES));
+        doc.add(new StringField("name", item.getString("Name"), Field.Store.YES));
+        String fullSearchableText = item.getString("Name") + " " + item.getString("Description") + category;
+
+        doc.add(new TextField("content", fullSearchableText, Field.Store.NO));
+        writer.addDocument(doc);
+    }
+
+    public void rebuildIndexes() throws SQLException, IOException {
 
         Connection conn = null;
 
         // create a connection to the database to retrieve Items from MySQL
-	try {
-	    conn = DbManager.getConnection(true);
-	} catch (SQLException ex) {
-	    System.out.println(ex);
-	}
+        try {
+           conn = DbManager.getConnection(true);
+       } catch (SQLException ex) {
+           System.out.println(ex);
+       }
 
-    getIndexWriter(true);
+       getIndexWriter(true);
 
-    Hotel[] hotels = HotelDatabase.getHotels();
-    for(Hotel hotel : hotels) {
-        indexHotel(hotel);
+       Statement s = conn.createStatement();
+       Statement sc = conn.createStatement();
+       ResultSet rs = s.executeQuery("SELECT * FROM Item");
+
+       while( rs.next() ){
+        int itemid = rs.getInt("ItemID");
+        String query = "SELECT * FROM ItemCategory Where ItemID = '" + Integer.toString(itemid) + "';"; 
+        ResultSet rsc = sc.executeQuery(query);
+        String category = "";
+        while(rsc.next())    
+            category = category  + " " + rsc.getString("Category");
+        rsc.close();
+        indexItem(rs, category);
     }
 
-	/*
-	 * Add your code here to retrieve Items using the connection
-	 * and add corresponding entries to your Lucene inverted indexes.
-         *
-         * You will have to use JDBC API to retrieve MySQL data from Java.
-         * Read our tutorial on JDBC if you do not know how to use JDBC.
-         *
-         * You will also have to use Lucene IndexWriter and Document
-         * classes to create an index and populate it with Items data.
-         * Read our tutorial on Lucene as well if you don't know how.
-         *
-         * As part of this development, you may want to add 
-         * new methods and create additional Java classes. 
-         * If you create new classes, make sure that
-         * the classes become part of "edu.ucla.cs.cs144" package
-         * and place your class source files at src/edu/ucla/cs/cs144/.
-	 * 
-	 */
-
+    rs.close();
+    sc.close();
+    s.close();
     closeIndexWriter();
 
         // close the database connection
-	try {
-	    conn.close();
-	} catch (SQLException ex) {
-	    System.out.println(ex);
-	}
-    }    
+    try {
+       conn.close();
+   } catch (SQLException ex) {
+       System.out.println(ex);
+   }
+}    
 
-    public static void main(String args[]) {
-        Indexer idx = new Indexer();
-        idx.rebuildIndexes();
-    }   
+public static void main(String args[]) throws SQLException, IOException {
+    Indexer idx = new Indexer();
+    idx.rebuildIndexes();
+}   
 }
